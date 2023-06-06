4fd176b661d7a20a8528d5184ce7d6abc56bb6d3
Juan Pablo Zaldumbide Proaño <jzaldumbide@gmail.com>
2023-06-06 07:58:42 -0500

    añadí 2 clases iniciales

--- src/figuras2D.java
+++ src/figuras2D.java
@@ -1,2 +1,54 @@
 public class figuras2D {
+    //triangulo equilatero y cuadrado
+    private int nLados;//número de lados
+    public float lado;//longitud del lado
+
+    //Constructor
+    public figuras2D(int nLados) {
+        this.nLados = nLados;
+    }
+
+    public figuras2D(float lado) {
+        this.lado = lado;
+    }
+
+    public figuras2D(int nLados, float lado) {
+        this.nLados = nLados;
+        this.lado = lado;
+    }
+
+    //Setters y getters
+    public float getLado() {
+        return lado;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
+
+
+    public int getnLados() {
+        return nLados;
+    }
+
+    public void setnLados(int nLados) {
+        this.nLados = nLados;
+    }
+
+
+    public float areaCuadrado(int lado){
+    return this.lado*this.lado;
+    };
+    private float perimetroCuadrado(){
+return 0;
+    };
+
+    private float areaTriangulo(){
+        return 0;
+
+    };
+    private float perimetroTriangulo(){
+        return 0;
+
+    };
 }
---
+++ src/menuPrincipal.java
@@ -0,0 +1,8 @@
+public class menuPrincipal {
+    public static void main(String[] args) {
+        figuras2D miFigura=new figuras2D(4,2);
+        miFigura.setLado(3);
+
+        System.out.println(miFigura.areaCuadrado(7));
+    }
+}
